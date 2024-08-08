0763db2fd1bf8f021af3f3889578d60460819c7e
york8536 <york8536@gmail.com>
2024-08-08 23:24:02 +0800

    20240808
    新增遊戲攻略測試

--- TestCase.py
+++ TestCase.py
@@ -16,7 +16,7 @@ def setup_driver():
         # options.add_argument("--auto-open-devtools-for-tabs")  # 開啟F12
         driver = webdriver.Chrome(options=options) # 建立ChromeDriver實例
         driver.maximize_window() # 視窗最大化
-        driver.implicitly_wait(8)  # 設置隱性等待
+        driver.implicitly_wait(10)  # 設置隱性等待
         driver.get("https://www.google.com.tw/?hl=zh_TW") # 取得網址
         sys.stdout.reconfigure(encoding='utf-8') # 套用UTF-8編碼
         return driver
@@ -59,10 +59,10 @@ def control_slider_ad(driver): # 切換輪播廣告
     try:
         get_prevArrow = driver.find_element(By.CLASS_NAME, "indexCarousel_samplePrevArrow__IVQan")
         get_prevArrow.click()
-        time.sleep(1.5)
+        time.sleep(1)
         get_nextArrow = driver.find_element(By.CLASS_NAME, "indexCarousel_sampleNextArrow__7zNz_")
         get_nextArrow.click()
-        time.sleep(1.5)
+        time.sleep(1)
     except NoSuchElementException as e:
         print(f"Error controlling 'slider ad': {e}")
 
@@ -73,53 +73,114 @@ def open_play(driver): # 打開立即玩
     except NoSuchElementException as e:
         print(f"Error opening 'Play Now': {e}")
 
-def open_news(driver): # 打開最新消息
+def open_news(driver):
     try:
         get_news = driver.find_element(By.XPATH, ".//nav[@class='navBar_navTopList__Xaa_z']/ul/li[2]")
         get_news.click()
-        # get_all = WebDriverWait(driver,timeout=3).until(EC.element_to_be_clickable((By.XPATH, ".//ul[@class='tab_owlTab__ib_3_']/li[1]"))) # 顯性等待
+        time.sleep(1)
+        # WebDriverWait(driver,timeout=3).until(EC.element_to_be_clickable((By.XPATH, ".//ul[@class='tab_owlTab__ib_3_']/li[1]"))) # 顯性等待
+        get_activity = driver.find_element(By.XPATH, ".//ul[@class='tab_owlTab__ib_3_']/li[2]")
+        get_activity.click()
+        time.sleep(1)
+        get_maintain = driver.find_element(By.XPATH, ".//ul[@class='tab_owlTab__ib_3_']/li[3]")
+        get_maintain.click()
+        time.sleep(1)
+        get_other = driver.find_element(By.XPATH, ".//ul[@class='tab_owlTab__ib_3_']/li[4]")
+        get_other.click()
+        time.sleep(1)
         get_all = driver.find_element(By.XPATH, ".//ul[@class='tab_owlTab__ib_3_']/li[1]")
         get_all.click()
-        # time.sleep(1)
+        time.sleep(1)
+    except NoSuchElementException as e:
+        print(f"開啟最新消息錯誤: {e}")
+
+def read_news(driver):
+    try:
         open_all = driver.find_elements(By.XPATH, ".//div[@class='announcement_page_ListContainer__8lxIu']/div")
         for news in open_all:
             news.click()
+            time.sleep(0.5)
             get_news_title = driver.find_element(By.XPATH, ".//div[@class='annModal_title__ztKHL']/h3")
-            print('最新消息標題 : '+get_news_title.text)
-            close_news = driver.find_element(By.XPATH, ".//button[@class='detailModal_closeBtn__mWsKW']")
+            print('最新消息標題 : '+get_news_title.text)     
+            close_news = driver.find_element(By.XPATH, ".//button[@class='detailModal_closeBtn__mWsKW']")     
             close_news.click()
-        
+    except NoSuchElementException as e:
+        print(f"查看最新消息錯誤: {e}")
+
+def check_newsInside(driver):
+    try:
+        open_all = driver.find_elements(By.XPATH, ".//div[@class='announcement_page_ListContainer__8lxIu']/div")
         open_all[0].click()
-        time.sleep(1.5)
+        time.sleep(1)
         copy_URL = driver.find_element(By.XPATH, ".//button[@class='detailModal_copyBtn__AvhU9']")
         copy_URL.click()
         WebDriverWait(driver,timeout=3).until(EC.visibility_of_element_located((By.XPATH, "//*[contains(text(), '複製成功')]")))
         get_next = driver.find_element(By.XPATH, ".//div[@class='detailModal_annFooter__BFFIW']/button[2]")
         get_next.click()
-        time.sleep(1.5)
+        time.sleep(1)
         get_previous = driver.find_element(By.XPATH, ".//div[@class='detailModal_annFooter__BFFIW']/button[1]")
         get_previous.click()
+        time.sleep(1)
         close_news = driver.find_element(By.XPATH, ".//button[@class='detailModal_closeBtn__mWsKW']")
         close_news.click()
-            
-        get_activity = driver.find_element(By.XPATH, ".//ul[@class='tab_owlTab__ib_3_']/li[2]")
-        get_activity.click()
-        # time.sleep(1)
-        get_maintain = driver.find_element(By.XPATH, ".//ul[@class='tab_owlTab__ib_3_']/li[3]")
-        get_maintain.click()
-        # time.sleep(1)
-        get_other = driver.find_element(By.XPATH, ".//ul[@class='tab_owlTab__ib_3_']/li[4]")
-        get_other.click()
-        # time.sleep(1)
     except NoSuchElementException as e:
-        print(f"Error opening 'News': {e}")
+        print(f"檢查最新消息內部按鈕錯誤: {e}")
 
-def open_newbie(driver): # 打開新手必看
+def test_news(driver):  # 測試最新消息
+    open_news(driver)
+    read_news(driver)
+    check_newsInside(driver)
+
+def open_newbie(driver, subTab): # 打開新手必看
     try:
         get_newbie = driver.find_element(By.XPATH, ".//nav[@class='navBar_navTopList__Xaa_z']/ul/li[3]")
         get_newbie.click()
+        match subTab:
+            case 'gameGuide':
+                get_gameGuide = driver.find_element(By.XPATH, ".//ul[@class='navBar_subMenu__cw8xl']/li[1]")
+                get_gameGuide.click()
+            case 'featuredGames':
+                get_featuredGames = driver.find_element(By.XPATH, ".//ul[@class='navBar_subMenu__cw8xl']/li[2]")
+                get_featuredGames.click()
+            case 'vipDescription':
+                get_vipDescription = driver.find_element(By.XPATH, ".//ul[@class='navBar_subMenu__cw8xl']/li[3]")
+                get_vipDescription.click()
+            case 'giftDescription':
+                get_giftDescription = driver.find_element(By.XPATH, ".//ul[@class='navBar_subMenu__cw8xl']/li[4]")
+                get_giftDescription.click()
     except NoSuchElementException as e:
-        print(f"Error opening 'Newbie Guide': {e}")
+        print(f"開啟新手必看錯誤: {e}")
+
+def open_gameGuide(driver): # 打開遊戲攻略
+    try:
+        open_newbie(driver, 'gameGuide')
+        time.sleep(1)
+
+        index = [0, -1]
+        for i in index:
+            try:
+                read_gameGuides = driver.find_elements(By.XPATH, ".//div[@class='gameGuidle_guidleList__XEg6B']/a")
+                read_gameGuides[i].click()
+                time.sleep(1)
+                get_gameGuides_title = driver.find_element(By.XPATH, ".//h2[@class='gameGuidle_h2Title__i1X6j aos-init aos-animate']/strong")
+                print(get_gameGuides_title.text)
+                goBack = driver.find_element(By.XPATH, "//*[contains(text(), '遊戲攻略')]")
+                goBack.click()
+                time.sleep(1)
+            except Exception as e:
+                print(f"閱讀遊戲攻略錯誤 {i}: {e}")
+    except Exception as e:
+        print(f"開啟遊戲攻略錯誤: {e}")
+
+
+def test_newbie(driver):
+    time.sleep(1)
+    open_newbie(driver, 'featuredGames')
+    time.sleep(1)
+    open_newbie(driver, 'vipDescription')
+    time.sleep(1)
+    open_newbie(driver, 'giftDescription')
+    time.sleep(1)
 
 def open_ranking(driver): # 打開活動排行
     try:
